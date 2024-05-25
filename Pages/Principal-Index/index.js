3a6682889211630fffc1d3fae856ba62bf3258db
xanderflores <alexanderflores_10_@hotmail.es>
2024-05-25 16:55:35 -0300

    Modificamos el nav y el footer para que sea responsivo

---
+++ Pages/Principal-Index/index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', (event)=>{
+     
+ const boton =document.getElementById('miBoton');
+
+ boton.addEventListener('click', ()=>{
+    window.location.href='../Clases/clases.html';
+
+ });
+});
+
+
+
+
